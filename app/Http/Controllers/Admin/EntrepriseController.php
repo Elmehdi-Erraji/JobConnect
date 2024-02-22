006e2e8b09c3cf6efc49a6d1373a47060d9fdf6f
itzibtihal <ibtihalboukhanchouch@gmail.com>
2024-02-22 14:51:25 +0100

    add entreprise

--- app/Http/Controllers/Admin/EntrepriseController.php
+++ app/Http/Controllers/Admin/EntrepriseController.php
@@ -5,94 +5,62 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Entreprise;
 use App\Http\Requests\CreateEntrepriseRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class EntrepriseController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
-    {
-        $entreprises = Entreprise::all();
+{
+    $entreprises = Entreprise::with('entrepriseRepre')->get();
+    return view('entreprises.index', compact('entreprises'));
+}
 
-        return view('entreprises.index', compact('entreprises'));
-    }
+public function create()
+{
+    return view('entreprises.create'); 
+}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('entreprises.create');
-    }
+public function store(CreateEntrepriseRequest $request)
+{
+    $validatedData = $request->validated();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\CreateEntrepriseRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(CreateEntrepriseRequest $request)
-    {
-        Entreprise::create($request->validated());
+        $entreprise = Entreprise::create($validatedData);
+        $entreprise->entrepriseRepre()->associate(User::find($validatedData['user_id']));
+        $entreprise->save();
+        return redirect()->route('entreprises.index')->with('success', 'Entreprise created successfully.');
+}
 
-        return redirect()->route('entreprises.index')
-            ->with('success', 'Entreprise created successfully.');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Entreprise  $entreprise
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Entreprise $entreprise)
+public function show(Entreprise $entreprise)
     {
         return view('entreprises.show', compact('entreprise'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Entreprise  $entreprise
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Entreprise $entreprise)
     {
         return view('entreprises.edit', compact('entreprise'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\CreateEntrepriseRequest  $request
-     * @param  \App\Models\Entreprise  $entreprise
-     * @return \Illuminate\Http\Response
-     */
     public function update(CreateEntrepriseRequest $request, Entreprise $entreprise)
     {
-        $entreprise->update($request->validated());
+        $validatedData = $request->validated();
 
-        return redirect()->route('entreprises.index')
-            ->with('success', 'Entreprise updated successfully.');
+        $entreprise->update($validatedData);
+
+       
+        if ($validatedData['user_id'] != $entreprise->user_id) {
+            $entreprise->entrepriseRepre()->associate(User::find($validatedData['user_id']));
+            $entreprise->save();
+        }
+
+        return redirect()->route('entreprises.index')->with('success', 'Entreprise updated successfully.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Entreprise  $entreprise
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Entreprise $entreprise)
     {
         $entreprise->delete();
 
-        return redirect()->route('entreprises.index')
-            ->with('success', 'Entreprise deleted successfully.');
+        return redirect()->route('entreprises.index')->with('success', 'Entreprise deleted successfully.');
     }
 }
